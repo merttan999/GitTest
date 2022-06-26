5467f125125b4219581b388d21e5e0765e9c4813
Mert Tan <merttan@Mert-MacBook-Pro.local>
2022-06-26 15:29:04 +0300

    My First Git Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Mert Tan on 26.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("git")
+    }
+
+
+}
+
